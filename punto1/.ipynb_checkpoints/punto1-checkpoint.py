9380031983eee75d81d65af220c6ce8bd55626f5
dagarzon1 <da.garzon1@uniandes.edu.co>
2018-12-07 16:47:12 +0000

    Finale

--- punto1/.ipynb_checkpoints/punto1-checkpoint.py
+++ punto1/.ipynb_checkpoints/punto1-checkpoint.py
@@ -14,31 +14,34 @@ s = 1.0
 mu = 0.0
 y = 1.0/( s * np.sqrt(2 * np.pi) ) * np.exp( - ( x - mu ) * ( x - mu) / ( 2.0 * s * s ) )
 
-j=0
 plt.figure(figsize=(11,4))
 plt.subplot(1,2,1)
 for i in k:
 	d = np.loadtxt(i)
 	plt.hist(d, alpha=0.3, density=True)
-	means[j] = np.mean(d)
-	varis[j] = np.std(d) ** 2.0
-	j +=1
 plt.plot(x,y)
 plt.ylabel('freq')
 plt.xlabel('x')
 
-mean_dat = np.mean(means)
-means = means - mean_dat
-means = means ** 2.0
-for i in range(2,8):
-	b = N / (i-1) * np.sum(means[:i])
-	w = 1/i * np.sum(varis[:i])
-	v = (N-1) * w / N + ( (i+1) * b / (i*N) )
+
+for i in range(2,1000):
+	m = 0
+	for j in k:
+		d = np.loadtxt(j)
+		means[m] = np.mean(d[:i])
+		varis[m] = np.std(d[:i]) ** 2.0
+		m+=1
+	mean_dat = np.mean(means)
+	means = means - mean_dat
+	means = means ** 2.0
+	b = i / (8-1) * np.sum(means)
+	w = 1/8 * np.sum(varis)
+	v = (i-1) * w / i + ( (8+1) * b / (8*i) )
 	V.append(v)
 
 plt.subplot(1,2,2)
-plt.plot(np.arange(2,8),V)
+plt.plot(np.arange(2,1000),V)
 plt.ylabel('Gelman-Rubin variable')
-plt.xlabel('Number of chains')
+plt.xlabel('Number of iterations')
 
 plt.savefig('punto1.png')
---
+++ punto2/punto2.py
@@ -0,0 +1,110 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import odeint
+
+data = np.loadtxt('datos_observacionales.dat')
+
+def dxdt(sigma, x, y, z):
+    return sigma * (y-x)
+def dydt(rho, x, y, z):
+    return x * (rho - z) - y
+def dzdt(beta, x, y, z):
+    return x * y - beta * z
+def model_odf(w,t,params):
+    dx = dxdt(params[0],w[0],w[1],w[2])
+    dy = dydt(params[1],w[0],w[1],w[2])
+    dz = dzdt(params[2],w[0],w[1],w[2])
+    dw = [dx,dy,dz]
+    return dw
+def model(t, params, w_in=data[0,1:]):
+    w_solve = odeint(model_odf,w_in,t, args=(params,))
+    x = w_solve[:,0]
+    y = w_solve[:,1]
+    z = w_solve[:,2]
+    return x,y,z
+def loglikelihood(t, f, param, sigma=1.0):
+    d = f.T -  model(t, param)
+    d = d/sigma
+    d = -0.5 * np.sum(d**2)
+    return d
+def logprior(param):
+    return np.sum(np.where(param>30,-np.inf,0))
+def H(t,f,param,momentum):
+    K = np.sum(momentum)** 2.0 / 2.0
+    V = - loglikelihood(t,f,param) - logprior(param)
+    return K + V
+def der_loglikelihood(t,f,param):
+    n = len(param)
+    res = []
+    d = 0.001
+    for i in range(n):
+        l_1 = loglikelihood(t,f,param+d)
+        l_2 = loglikelihood(t,f,param-d)
+        res.append((l_1+l_2)/2*d)
+    res = np.array(res)
+    return res
+def leapfrog(t,f,param,momentum):
+    dt = 0.0001
+    param_n = np.array(param)
+    momentum_n = np.array(momentum)
+    for i in range(3):
+        momentum_n = momentum_n + der_loglikelihood(t,f,param) * dt/2.0
+        param_n = param_n + momentum * dt
+        momentum_n = momentum_n + der_loglikelihood(t,f,param) * dt/2.0
+    momentum_n = - momentum_n
+    return param_n, momentum_n
+def MC(d):
+    param = [np.random.random(3)]
+    momentum = [np.random.normal(loc=2, size=3)]
+    t = d[:,0][1:]
+    w = d[:,1:4][1:]
+    for i in range(1,1000):
+        p , m = leapfrog(t,w,param[i-1], momentum[i-1])
+        E_new = H(t,w,p,m)
+        E_old = H(t,w,param[i-1],momentum[i-1])
+        
+        alpha = np.random.random()
+        r = np.exp(-(E_new-E_old))
+        #print(p,m)
+        if (r>alpha):
+            param.append(p)
+        else:
+            param.append(param[i-1])
+        momentum.append(np.random.normal(loc=2,size=3))
+    return param
+
+p = np.array(MC(data))
+
+plt.figure(figsize=(11,4))
+
+plt.subplot(1,3,1)
+_ = plt.hist(p[:,0])
+m = np.mean(p[:,0])
+s = np.std(p[:,0])
+plt.title('sigma\n'+'mean='+str(m)+'\n'+'std='+str(s))
+
+plt.subplot(1,3,2)
+_ = plt.hist(p[:,1])
+m = np.mean(p[:,1])
+s = np.std(p[:,1])
+plt.title('rho\n'+'mean='+str(m)+'\n'+'std='+str(s))
+
+plt.subplot(1,3,3)
+_ = plt.hist(p[:,2])
+m = np.mean(p[:,2])
+s = np.std(p[:,2])
+plt.title('beta\n'+'mean='+str(m)+'\n'+'std='+str(s))
+
+plt.savefig('histogramas.png')
+
+plt.clf()
+
+t = data[:,0]
+x = data[:,1]
+y = data[:,2]
+z = data[:,3]
+plt.plot(t,x,label='x')
+plt.plot(t,y,label='y')
+plt.plot(t,z,label='z')
+plt.legend()
+plt.savefig('modelo.png')
